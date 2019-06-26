77afb3fc576b9cf50eb46ddbdaa95ba31350ceba
Ramon Anglada <anglada.alex@gmail.com>
2019-06-26 17:53:10 -0400

    updated datanode and namenode dockerfiles

--- docker-hadoop/build-push-docker-hub.sh
+++ docker-hadoop/build-push-docker-hub.sh
@@ -6,10 +6,10 @@ HADOOP_VERSION=2.9.2
 
 docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION -t $HUBUSER/hadoop-base:$HADOOP_VERSION .
 
-docker build -f nn-rm/Dockerfile -t $HUBUSER/hadoop-namenode .
+docker build -f nn-rm/Dockerfile -t $HUBUSER/hadoop-namenode:$HADOOP_VERSION .
 #docker build -f nn-rm/Dockerfile -t $HUBUSER/hadoop-namenode --build-args HDFS_PATH_TO_DATA=$HDFS_PATH_TO_DATA .
 
-docker build -f datanode/Dockerfile -t $HUBUSER/hadoop-datanode .
+docker build -f datanode/Dockerfile -t $HUBUSER/hadoop-datanode:$HADOOP_VERSION .
 #docker build -f datanode/Dockerfile -t $HUBUSER/hadoop-datanode --build-args HDFS_PATH_TO_DATA=$HDFS_PATH_TO_DATA .
 
 #TODO docker loing to docker hub
