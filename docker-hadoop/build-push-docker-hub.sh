69f7cf74c70af8aacc28446f934268ee934adeb5
Ramon Anglada <anglada.alex@gmail.com>
2019-07-04 11:18:35 -0400

    updated build bash script for docker hub

--- docker-hadoop/build-push-docker-hub.sh
+++ docker-hadoop/build-push-docker-hub.sh
@@ -8,23 +8,17 @@ docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION -t $HUBUSER/hadoop-base:
 docker-compose build
 docker-compose up -d
 
-
-#docker build -f nn-rm/Dockerfile -t $HUBUSER/hadoop-namenode:$HADOOP_VERSION .
-#docker build -f nn-rm/Dockerfile -t $HUBUSER/hadoop-namenode --build-args HDFS_PATH_TO_DATA=$HDFS_PATH_TO_DATA .
-
-#docker build -f datanode/Dockerfile -t $HUBUSER/hadoop-datanode:$HADOOP_VERSION .
-#docker build -f datanode/Dockerfile -t $HUBUSER/hadoop-datanode --build-args HDFS_PATH_TO_DATA=$HDFS_PATH_TO_DATA .
-
-#TODO docker login docker hub
-
+#docker login docker hub
 #docker loging --username=$HUBUSER --password:xxxxxxxx
 
-# docker push $HUBUSER/hadoop-base:$HADOOP_VERSION
+#tagging
+docker tag $HUBUSER/hadoop-namenode $HUBUSER/hadoop-namenode:$HADOOP_VERSION
+docker tag $HUBUSER/hadoop-datanode $HUBUSER/hadoop-datanode:$HADOOP_VERSION
 
-# docker push $HUBUSER/hadoop-namenode:$HADOOP_VERSION
-
-# docker push $HUBUSER/hadoop-datanode:$HADOOP_VERSION
-
+#pushing to docker hub
+docker push $HUBUSER/hadoop-base:$HADOOP_VERSION
+docker push $HUBUSER/hadoop-namenode:$HADOOP_VERSION
+docker push $HUBUSER/hadoop-datanode:$HADOOP_VERSION
 
 
 
